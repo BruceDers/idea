35f5055529384fefb32959e074f747d4041e5a47
DerAeri <15514980881@163.com>
2022-03-14 13:59:24 +0800

    添加

---
+++ src/com/user/java/New2.java
@@ -0,0 +1,11 @@
+package com.user.java;
+
+/**
+ * @author Administrator
+ * @create 2022-03-14-13:58
+ */
+public class New2 {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
